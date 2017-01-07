5a0081eca9d13ee9e0cc30d5dd69d21e0b6e6569
Charlie Dowley <dowleycharlotte@hotmail.co.uk>
2017-01-07 01:26:10 +0000

    New Timeline Build
    Timeline Build, JS & CSS

---
+++ js/timeline-attributes.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	
+	$('.timeline').timeline({
+			animLeft: "fadeInLeft",
+		animCenter: "fadeInUp",
+		animRight: "fadeInRight",
+		animSpeed: 600,
+		offset: 150
+	});
+	
+	
+})
